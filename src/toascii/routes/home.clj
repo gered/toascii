bb39d4e1b7c8aa67301db9dcd179b967eaab940c
gered <gered@blarg.ca>
2014-03-30 17:13:57 -0400

    pass list of flf font names to the main template

--- src/toascii/routes/home.clj
+++ src/toascii/routes/home.clj
@@ -1,11 +1,13 @@
 (ns toascii.routes.home
   (:require [toascii.route-utils :refer [register-routes]]
-            [toascii.views.layout :as layout])
+            [toascii.views.layout :as layout]
+            [toascii.models.flf :refer [get-font-names]])
   (:use compojure.core))
 
 (defn home-page []
   (layout/render
-    "home.html"))
+    "home.html"
+    :params {:fontNames (get-font-names)}))
 
 (register-routes home-routes
   (GET "/" [] (home-page)))

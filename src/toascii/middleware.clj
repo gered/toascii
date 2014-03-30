65baca1e7ce8c55158d02cfa41d94903ebbebdbe
gered <gered@blarg.ca>
2014-03-30 16:49:04 -0400

    add site error template. update wrap-exceptions to nicely handle api errors and general "web page" errors

--- src/toascii/middleware.clj
+++ src/toascii/middleware.clj
@@ -1,12 +1,27 @@
 (ns toascii.middleware
-  (:require [taoensso.timbre :refer [log]]))
+  (:require [taoensso.timbre :refer [log]]
+            [toascii.util :refer [get-throwable-stack-trace]]
+            [toascii.views.layout :as layout]))
+
+(defn api-request? [request]
+  (let [api-route-prefix (-> (:context request)
+                             (str "/api")
+                             (.replace "//" "/"))
+        uri              (:uri request)]
+    (.startsWith uri api-route-prefix)))
 
 (defn wrap-exceptions [handler]
   (fn [request]
     (try
       (handler request)
       (catch Exception ex
-        (log :error ex "Unhandled exception lol.")
-        (println (:uri request) (:context request))
-        {:status 500
-         :body "An error occurred! oh noes!"}))))
+        (log :error ex "Unhandled exception.")
+        (if (api-request? request)
+          {:status 500
+           :content-type "text/plain"
+           :body (.toString ex)}
+          (layout/render-response
+            request
+            "error.html"
+            :params {:stacktrace (get-throwable-stack-trace ex)}
+            :status 500))))))

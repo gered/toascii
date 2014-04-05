1732ccf341f6bbf3399744d0e31c9114829b0f16
gered <gered@blarg.ca>
2014-04-05 19:48:41 -0400

    add response streaming to gif->ascii

--- src/toascii/models/image.clj
+++ src/toascii/models/image.clj
@@ -1,8 +1,10 @@
 (ns toascii.models.image
   (:import (java.awt.image BufferedImage)
+           (java.io Writer)
            (javax.imageio.stream ImageInputStream))
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
+            [ring.util.io :refer [piped-input-stream]]
             [clj-image2ascii.core :as i2a]
             [toascii.util :refer [query-param-url->java-url]])
   (:use hiccup.core))
@@ -19,7 +21,7 @@
   (let [java-url (query-param-url->java-url url)]
     (i2a/get-image-stream-by-url java-url)))
 
-(defn wrap-pre-tag [s & {:keys [hidden? delay]}]
+(defn- wrap-pre-tag [s & {:keys [hidden? delay]}]
   (str
     "<pre style=\""
     ascii-pre-css
@@ -30,6 +32,17 @@
       (str " data-delay=\"" delay "\""))
     ">" s "</pre>"))
 
+(defn- get-open-pre-tag [hidden? delay]
+  (str
+    "<pre style=\""
+    ascii-pre-css
+    (if hidden?
+      " display: none;")
+    "\""
+    (if delay
+      (str " data-delay=\"" delay "\""))
+    ">"))
+
 (defn image->ascii [^BufferedImage image scale-to-width color? html?]
   (let [converted (i2a/convert-image image scale-to-width color?)
         ascii     (:image converted)]
@@ -37,14 +50,21 @@
       (wrap-pre-tag ascii)
       ascii)))
 
+(defn- write-out [^Writer w s]
+  (.write w (str s)))
+
 (defn gif->ascii [^ImageInputStream image-stream scale-to-width color?]
-  (as-> image-stream x
-        (i2a/convert-animated-gif-frames x scale-to-width color?)
-        (:frames x)
-        (map
-          (fn [frame]
-            (wrap-pre-tag (:image frame) :delay (:delay frame) :hidden? true))
-          x)
-        (str/join x)
-        (str "<div class=\"animated-gif-frames\">" x "</div>")
-        (str x "<script type=\"text/javascript\">" js-gif-animation "</script>")))
+  (piped-input-stream
+    (fn [output-stream]
+      (with-open [^Writer w (io/writer output-stream :append true)]
+        (write-out w "<div class=\"animated-gif-frames\">")
+        (i2a/stream-animated-gif-frames!
+          image-stream scale-to-width color?
+          (fn [{:keys [image delay]}]
+              (write-out w (get-open-pre-tag true delay))
+              (write-out w image)
+              (write-out w "</pre>")))
+        (write-out w "</div>")
+        (write-out w "<script type=\"text/javascript\">")
+        (write-out w js-gif-animation)
+        (write-out w "</script>")))))

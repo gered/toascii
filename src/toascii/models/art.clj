335009597d825431e6942eaaa16b3774c4145cf7
gered <gered@blarg.ca>
2014-04-06 13:22:46 -0400

    add stub functions for all the main operations we need

---
+++ src/toascii/models/art.clj
@@ -0,0 +1,17 @@
+(ns toascii.models.art
+  (:require [toascii.models.db :as db]))
+
+(defn search [query]
+  )
+
+(defn get-count [name]
+  )
+
+(defn get-index [name index]
+  )
+
+(defn get-random [name]
+  )
+
+(defn add [name s ip]
+  )

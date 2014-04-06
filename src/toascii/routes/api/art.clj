df0f6d7429036bb0c1307e6c168d6d6d41e4d6d0
gered <gered@blarg.ca>
2014-04-06 17:04:35 -0400

    add resource handler for random ascii art under a given name

--- src/toascii/routes/api/art.clj
+++ src/toascii/routes/api/art.clj
@@ -18,5 +18,35 @@
     (:error ctx))
   )
 
+(defresource get-random-art [{:keys [name format]}]
+  :media-type-available?
+  (fn [ctx]
+    (let [type (condp = format
+                 "html" "text/html"
+                 "text" "text/plain"
+                 "text/plain")]
+      {:representation {:media-type type}}))
+  :malformed?
+  (fn [_]
+    (if-not (art/valid-name? name)
+      {:error "Invalid name."}))
+  :exists?
+  (fn [_]
+    (if-let [ascii (art/get-random name)]
+      {:ascii ascii}
+      [false {:error "No art found under that name."}]))
+  :handle-ok
+  (fn [ctx]
+    (if (= "text/html" (get-in ctx [:representation :media-type]))
+      (str "<pre>" (:ascii ctx) "</pre>")
+      (:ascii ctx)))
+  :handle-malformed
+  (fn [ctx]
+    (:error ctx))
+  :handle-not-found
+  (fn [ctx]
+    (:error ctx)))
+
 (register-routes api-art-routes
+  (ANY "/api/art/:name" {params :params} (get-random-art params))
   (ANY "/api/art" {{q :q} :params} (art-search q)))
